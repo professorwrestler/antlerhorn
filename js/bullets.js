71f462619d539605169bfb3b6ab43a531b34b873
professorwrestler <138532664+professorwrestler@users.noreply.github.com>
2024-06-21 18:01:12 -0400

    Prettified code.

--- js/bullets.js
+++ js/bullets.js
@@ -1,33 +1,27 @@
-class Bullet extends Phaser.GameObjects.Sprite
-{
-    constructor (scene)
-    {
-        super(scene, 0, 0, 'flame');
-        // add physics so overlap (collision) works
-        scene.physics.add.existing(this);
-        this.speed = Phaser.Math.GetSpeed(600, 1);
+class Bullet extends Phaser.GameObjects.Sprite {
+  constructor(scene) {
+    super(scene, 0, 0, "flame");
+    // add physics so overlap (collision) works
+    scene.physics.add.existing(this);
+    this.speed = Phaser.Math.GetSpeed(600, 1);
+  }
 
+  fire(x, y) {
+    this.setPosition(x, y);
+
+    this.setActive(true);
+    this.setVisible(true);
+  }
+
+  update(time, delta) {
+    this.x += this.speed * delta;
+    //if the bullet's X position gets greater than 258 pixels,
+    if (this.x > 256) {
+      //deactivate, destroy, and log to the console that it's gone
+      this.setActive(false);
+      this.setVisible(false);
+      console.log("bullet gone");
+      this.destroy();
     }
-
-    fire (x, y)
-    {
-        this.setPosition(x, y);
-
-        this.setActive(true);
-        this.setVisible(true);
-    }
-
-    update (time, delta)
-    {
-        this.x += this.speed * delta;
-        //if the bullet's X position gets greater than 258 pixels, 
-        if (this.x > 256)
-        {
-            //deactivate, destroy, and log to the console that it's gone
-            this.setActive(false);
-            this.setVisible(false);
-            console.log("bullet gone");
-            this.destroy();
-        }
-    }
+  }
 }
